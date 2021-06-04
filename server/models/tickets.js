08538d29d2c1b885874285b58d0a13c6e05e5924
fatih <fato@noitso.dk>
2021-06-04 04:56:30 +0200

    added ticket

--- server/models/tickets.js
+++ server/models/tickets.js
@@ -4,13 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class tickets extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
     }
   };
   tickets.init({
@@ -22,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
     seat: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'tickets',
+    modelName: 'Tickets',
   });
   return tickets;
 };
---
+++ server/routes/ticket.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+
+var { Tickets } = require('../models')
+
+router.get('/', async function(req, res, next) {
+    await Tickets.findAll()
+    .then(response => {
+        res.send({response, status:"OK"})
+    }).catch(err=> {
+        res.send({err, status:"ERROR"})
+    })
+});
+
+router.get('/:id', async function(req, res, next) {
+    await Theater.findOne({ where: {theaterId: req.params.id}})
+    .then(response => {
+        console.log(req.params.id)
+        res.send({response, status:"OK"})
+    }).catch(err=> {
+        res.send({err, status:"ERROR"})
+    })
+});
+
+router.post('/', async function(req, res, next) {
+    await Theater.create({
+        rowQty: req.body.normalrow,
+        vipRowQty: req.body.viprow,
+        hallId: req.body.selecthall,
+        movieId: req.body.selectmovie
+    }).then(result => {
+        return res.send({result, status: "OK"})
+    }).catch(err => {
+        return res.send({err})
+    })
+});
+
+router.put('/:id', async function(req, res, next) {
+    await Theater.findOne({where:{ theaterId: req.params.id }})
+    .then(async function(theater) {
+        if (theater == null) { 
+            return res.send({ message:"Theater findes ikke!", status: "ERROR" })
+        }
+        await Theater.update({
+            rowQty: req.body.qty,
+            vipRowQty: req.body.availabel,
+            hallId: req.body.theaterId,
+            movieId: req.body.movieId
+        }, { 
+            where:{
+                theaterId: req.params.id
+            }
+        }).then(resp => {
+            res.send({ status: "OK" })
+        }).catch(error => {
+            res.send({ status: "error", message: error })
+        })
+    })
+});
+
+router.delete('/:id', async (req, res, next) => {
+    await Theater.findOne({ where:{ theaterId: req.params.id }})
+    .then(async function(theater) {
+        if (!theater) {
+            return res.send({ message:"Theater findes ikke!", status: "ERROR" })
+        }
+        await Theater.destroy({
+            where:{ theaterId: theater.theaterId }
+        }).then(resp => {
+            res.send({ status: "OK" })
+        }).catch(error => {
+            res.send({ status: "error", message: error })
+        })
+    })
+});
+
+module.exports = router;
